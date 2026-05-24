e9d2db8779254c579d06a80dce76f32cfb8a3c3b
johnflowerful@users.noreply.github.com <johnflowerful@users.noreply.github.com>
2026-05-24 13:14:50 +1200

    _scripts: remove cruft

--- _scripts/roon_current_ver.sh
+++ _scripts/roon_current_ver.sh
@@ -1,175 +1,61 @@
-#/bin/sh
-set -o errexit -o pipefail -o noclobber -o nounset
+#!/usr/bin/env bash
 
-function show_help() {
-	echo "usage: roon_current_ver [OPTION]"
-	echo "display information about the current RoonServer release (version only by default)"
-	echo
-	echo "-a, --all            output both executable and stripped file information"
-	echo "-d, --dir            set the working directory (/var/tmp by default)"
-	echo "-e, --executables    output a list of files that have executable bit set (excludes font files)"
-	echo "-s, --stripped       output a list of files that have stripped symbols"
-	echo "-h, --help           show this help message"
-	echo "-o, --out            print the output to files instead"
-	echo
-	echo "--keep-files         do not remove tarball and the extracted files"
+set -o errexit -o pipefail -o nounset
 
-	exit 0
-}
-
-! getopt --test > /dev/null 
-if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-	echo "`getopt --test` failed in this environment."
-	exit 1
-fi
-
-OPTS=ad:ehos
-LONGOPTS=all,dir:,executables,help,keep-files,out,stripped
-
-! PARSED=$(getopt --options=$OPTS --longoptions=$LONGOPTS --name "$0" -- "$@")
-if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-	# e.g. return value is 1
-	#  then getopt has complained about wrong arguments to stdout
-	exit 2
-fi
-
-# read getopt's output this way to handle the quoting right:
-eval set -- "$PARSED"
-
-directory= exec= keep= out= strip=
-# now enjoy the options in order and nicely split until we see --
-while true; do
-	case "$1" in
-		-a|--all)
-			exec=y strip=y
-			shift;;
-		-d|--dir)
-			directory="$2"
-			shift 2;;
-		-e|--executables)
-			exec=y
-			shift;;
-		-h|--help)
-			show_help
-			break;;
-		--keep-files)
-			keep=y
-			shift;;
-		-o|--output)
-			out=y
-			shift;;
-		-s|--stripped)
-			strip=y
-			shift;;
-		--)
-			shift
-			break;;
-		*)
-			echo "Programming error"
-			exit 3;;
-	esac
-done
-
+TEMPDIR="/var/tmp"
 
 ROONSERVER_BASEURL="http://download.roonlabs.com/builds/"
 ROONSERVER_FILENAME="RoonServer_linuxx64.tar.bz2"
-TEMPDIR="/var/tmp"
+FILE_PATH="${TEMPDIR}/${ROONSERVER_FILENAME}"
 
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[0;33m"
-NC="\033[0m" # no colour
-
-PREFIX="${GREEN}*${NC} "
-ERROR_PREFIX="${RED}*${NC} "
-WARN_PREFIX="${YELLOW}*${NC} "
-
-WORKDIR=${directory}
-
-if ! [[ ${directory} ]]; then
-	echo "${PREFIX}Using ${TEMPDIR} as work directory"
-	WORKDIR=${TEMPDIR}
+RED="" GREEN="" YELLOW="" RESET_ATTS="" ALERT_TEXT=""
+if [[ -v TERM && -n "${TERM}" && "${TERM}" != "dumb" ]]; then
+	RED="$(tput setaf 1)$(tput bold)"
+	GREEN="$(tput setaf 2)$(tput bold)"
+	YELLOW="$(tput setaf 3)$(tput bold)"
+	RESET_ATTS="$(tput sgr0)"
+	ALERT_TEXT="$(tput bel)"
 fi
+PREFIX_STRING="* "
+OUTPUT_PREFIX="${GREEN}${PREFIX_STRING}${RESET_ATTS}"
+QUERY_STRING="> "
+QUERY_PREFIX="${GREEN}${QUERY_STRING}${RESET_ATTS}"
 
-if ! [[ -d ${WORKDIR} ]]; then
-	echo "${ERROR_PREFIX}$0: error: ${WORKDIR} does not exist"
+warning() {
+	echo -e "${YELLOW}${PREFIX_STRING}${RESET_ATTS}Warning: ${1}" >&2
+}
+
+die() {
+	echo -e "${RED}${PREFIX_STRING}${RESET_ATTS}Error: ${1} - exiting" >&2
 	exit 1
-fi
+}
 
-cd ${WORKDIR}
-
-echo -e "${PREFIX}downloading ${RED}${ROONSERVER_FILENAME}${NC}"
-if [[ -f "$ROONSERVER_FILENAME" ]]; then
-	echo -e "${WARN_PREFIX}${RED}${ROONSERVER_FILENAME} ${NC}already exists."
-	while true; do
-		echo -ne "do you want to redownload ${RED}${ROONSERVER_FILENAME}${NC}? "
-		read yn
-		case $yn in
-			[Yy]* )
-				rm -f ${ROONSERVER_FILENAME}
-				wget ${ROONSERVER_BASEURL}${ROONSERVER_FILENAME}
-				break;;
-			[Nn]* )
-				break;;
-			* )
-				echo "Please answer yes or no.";;
+test_yn_need_enter() {
+	while read -rp "${QUERY_PREFIX}${1} (y/n)? ${ALERT_TEXT}" yn; do
+		case "$yn" in
+			[Yy]* ) return 0;;
+			[Nn]* ) return 1;;
+			* ) echo -e "${YELLOW}${PREFIX_STRING}${RESET_ATTS}Please answer yes or no.";;
 		esac
 	done
-else
-	wget "http://download.roonlabs.com/builds/"${ROONSERVER_FILENAME}
-fi
+}
 
-echo -e "${PREFIX}extracting ${RED}${ROONSERVER_FILENAME} ${NC}to ${WORKDIR}"
-if [[ -d "${WORKDIR}/RoonServer" ]]; then
-	echo -e "${WARN_PREFIX}warning: ${WORKDIR}RoonServer already exists."
-	while true; do
-		read -p "do you want to use existing files? " yn
-		case $yn in
-			[Yy]* )
-				break;;
-			[Nn]* )
-				echo -e "${PREFIX}removing old files"
-				rm -rf ${WORKDIR}/RoonServer
-				echo -e "${PREFIX}extracting..."
-				tar xjf ${ROONSERVER_FILENAME}
-				break;;
-			* )
-				echo "Please answer yes or no.";;
-		esac
-	done
-else
-	tar xjf ${ROONSERVER_FILENAME}
-fi
-
-if [[ $exec ]]; then
-	echo -e "${GREEN}executables:${NC}"
-	executables=$(find RoonServer/ ! \( -name "*.otf" -o -name "*.ttf" \) -executable -type f | sed -r "s/RoonServer\///" | sort)
-	if [[ $out ]]; then
-		echo -e "${PREFIX}writing roon_executables.txt"
-		printf "%s" "$executables" >| roon_executables.txt
-	else
-		echo "$executables"
+DOWNLOAD=true
+if [[ -f "${FILE_PATH}" ]]; then
+	warning "${RED}${ROONSERVER_FILENAME}${RESET_ATTS} already exists."
+	if ! test_yn_need_enter "do you want to redownload it"; then
+		DOWNLOAD=false
 	fi
-	echo -e "\n"
+fi
+if ${DOWNLOAD}; then
+	rm -fv "${FILE_PATH}"
+	wget -P ${TEMPDIR} "${ROONSERVER_BASEURL}${ROONSERVER_FILENAME}"
 fi
 
-if [[ $strip ]]; then
-	echo -e "${GREEN}stripped files:${NC}"
-	stripped=$(find RoonServer/ -type f \( -name "*.so" -o -executable \) ! \( -name "*.otf" -o -name "*.ttf" -o -name "*.dll" \) -exec sh -c 'objdump --syms {} | grep -q "no symbols" && echo "{}"' \;  2>/dev/null | sed -r "s/RoonServer\///" | sort)
-	if [[ $out ]]; then
-		echo -e "${PREFIX}writing roon_stripped.txt"
-		printf "%s" "$stripped" >| roon_stripped.txt
-	else
-		echo "$stripped"
-	fi
-	echo -e "\n"
-fi
-
-echo -e "${GREEN}RoonServer version:${NC}"
-cat RoonServer/VERSION
+echo -e "${GREEN}RoonServer version:${RESET_ATTS}"
+tar xjf "${FILE_PATH}" --to-stdout RoonServer/VERSION
 echo -e "\n"
 
-if ! [[ $keep ]]; then
-	rm -rf ${WORKDIR}/RoonServer
-	rm -i ${ROONSERVER_FILENAME}
+if test_yn_need_enter "do you want delete ${FILE_PATH}"; then
+	rm -fv "${FILE_PATH}"
 fi
